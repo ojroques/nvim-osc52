c6d611c480cf44f632d216268341d0aaaf726daf
Olivier Roques <git@oroques.dev>
2022-07-17 20:33:12 +0200

    feat: add visual support

--- lua/oscyank.lua
+++ lua/oscyank.lua
@@ -1,20 +1,24 @@
 -------------------- VARIABLES -----------------------------
 local base64 = require('base64')
 local fmt = string.format
+local commands = {
+  operator = {block = "`[\\<C-v>`]y", char = "`[v`]y", line = "'[V']y"},
+  visual = {['V'] = "y", ['v'] = "y", [''] = "y"},
+}
 local options = {
   max_length = 1000000,  -- Maximum length of selection
   silent = false,        -- Disable message on successful copy
+  trim = true,           -- Trim text
 }
 local M = {}
 
--------------------- UTILS ---------------------------------
+-------------------- CORE ----------------------------------
 local function echo(text, hl_group)
   vim.api.nvim_echo({{fmt('[oscyank] %s', text), hl_group or 'Normal'}}, false, {})
 end
 
-local function get_text(type)
-  local types = {block = "`[\\<C-v>`]y", char = "`[v`]y", line = "'[V']y"}
-  local command = fmt('noautocmd keepjumps normal! %s', types[type])
+local function get_text(mode, type)
+  local command = fmt('noautocmd keepjumps normal! %s', commands[mode][type])
   local text = ''
 
   -- Save user settings
@@ -23,11 +27,16 @@ local function get_text(type)
   local register = vim.fn.getreginfo('"')
   local visual_marks = {vim.fn.getpos("'<"), vim.fn.getpos("'>")}
 
-  -- Copy text
+  -- Retrieve text
   vim.go.clipboard = ''
   vim.go.selection = 'inclusive'
   vim.cmd(fmt('silent execute "%s"', command))
   text = vim.fn.getreg('"')
+
+  -- Trim text
+  if options.trim then
+    text = vim.trim(text)
+  end
 
   -- Restore user settings
   vim.go.clipboard = clipboard
@@ -40,21 +49,14 @@ local function get_text(type)
 end
 
 local function osc52(text)
-  local text_b64 = base64.enc(text)
-  local osc = fmt([[%s]52;c;%s%s]], string.char(0x1b), text_b64, string.char(0x07))
-  return vim.fn.chansend(vim.v.stderr, osc)
-end
-
--------------------- CORE ----------------------------------
-function M.copy_callback(type)
-  local text = get_text(type)
-
   if #text > options.max_length then
     echo(fmt('Selection is too big: length is %d, limit is %d', #text, options.max_length), 'WarningMsg')
     return
   end
 
-  local success = osc52(text)
+  local text_b64 = base64.enc(text)
+  local osc = fmt([[%s]52;c;%s%s]], string.char(0x1b), text_b64, string.char(0x07))
+  local success = vim.fn.chansend(vim.v.stderr, osc)
 
   if not success then
     echo('Failed to copy selection', 'ErrorMsg')
@@ -63,9 +65,20 @@ function M.copy_callback(type)
   end
 end
 
-function M.copy()
-  vim.go.operatorfunc = "v:lua.require'oscyank'.copy_callback"
+-------------------- PUBLIC --------------------------------
+function M.operator_copy_cb(type)
+  local text = get_text('operator', type)
+  osc52(text)
+end
+
+function M.operator_copy()
+  vim.go.operatorfunc = "v:lua.require'oscyank'.operator_copy_cb"
   return 'g@'
+end
+
+function M.visual_copy()
+  local text = get_text('visual', vim.fn.visualmode())
+  osc52(text)
 end
 
 -------------------- SETUP ---------------------------------
